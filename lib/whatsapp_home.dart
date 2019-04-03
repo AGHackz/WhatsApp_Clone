fc6e94d7fb8a2320274c2e7585ea65019f90a048
Ankit Gupta <ankit4579@gmail.com>
2019-04-04 00:17:22 +0530

    new tabbar added

--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -1,19 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/camera_screen.dart';
+import 'package:whatsapp_clone/chats_screen.dart';
+import 'package:whatsapp_clone/status_screen.dart';
+import 'package:whatsapp_clone/calls_screen.dart';
 
 class WhatsAppHome extends StatefulWidget {
   @override
   _WhatsAppHomeState createState() => _WhatsAppHomeState();
 }
 
-class _WhatsAppHomeState extends State<WhatsAppHome> {
+class _WhatsAppHomeState extends State<WhatsAppHome>
+    with TickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, initialIndex: 1, length: 4);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("WhatsApp"),
         elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            )
+          ],
+        ),
       ),
-      body: Container(),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new CameraScreen(),
+          new ChatsScreen(),
+          new StatusScreen(),
+          new CallsScreen()
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).accentColor,
+        child: Icon(Icons.message),
+        onPressed: () {
+          print("Open Contacts");
+        },
+      ),
     );
   }
 }
